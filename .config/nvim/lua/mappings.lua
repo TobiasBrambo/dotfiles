0abdec9d04b09a73911e93b3ae199bae529a794b
TobiasMB <tobias.m.brambo@gmail.com>
2024-10-19 23:49:37 +0200

    added keybinds for moving between tmux and nvim better

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,3 +9,15 @@ map("i", "jk", "<ESC>")
 map("i", "kj", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+local unmap = vim.keymap.del
+
+--- This fixes an issue with the vim-tmux-navigator + nvchad in which the base nvchad
+--- mapping were conflicting with vim-tmux-navigator ones.
+unmap("n", "<c-h>")
+unmap("n", "<c-j>")
+unmap("n", "<c-k>")
+unmap("n", "<c-l>")
+map("n", "<c-h>", "<cmd>:NvimTmuxNavigateLeft<cr>")
+map("n", "<c-j>", "<cmd>:NvimTmuxNavigateDown<cr>")
+map("n", "<c-k>", "<cmd>:NvimTmuxNavigateUp<cr>")
+map("n", "<c-l>", "<cmd>:NvimTmuxNavigateRight<cr>")
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -20,6 +20,22 @@ return {
     end,
     requires = {"nvim-lua/plenary.nvim"}
   },
-
+  {
+    'alexghergh/nvim-tmux-navigation',
+    lazy=false,
+    config = function()
+    require'nvim-tmux-navigation'.setup {
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+            left = "<C-h>",
+            down = "<C-j>",
+            up = "<C-k>",
+            right = "<C-l>",
+            last_active = "<C-\\>",
+            next = "<C-Space>",
+        }
+    }
+  end
+  }
 
 }
